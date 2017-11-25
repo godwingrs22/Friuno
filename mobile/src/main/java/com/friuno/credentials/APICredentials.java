9230c53813575a59ed352ca68d5a00357b879fe0
Godwin Rose Samuel <godwingrs22@gmail.com>
2017-11-25 11:04:46 +0530

    remove

--- mobile/src/main/java/com/friuno/credentials/APICredentials.java
+++
@@ -1,9 +0,0 @@
-package com.friuno.credentials;
-
-/**
- * Created by GodwinRoseSamuel on 15-09-2016.
- */
-public class APICredentials {
-    public static final String FACE_DETECTION_ENDPOINT = "https://api.idolondemand.com/1/api/sync/detectfaces/v1";
-    public static final String HP_IDOL_ACCESS_KEY = "7eecdbad-101d-4fcd-bfb8-17320bb6aba3";
-}
